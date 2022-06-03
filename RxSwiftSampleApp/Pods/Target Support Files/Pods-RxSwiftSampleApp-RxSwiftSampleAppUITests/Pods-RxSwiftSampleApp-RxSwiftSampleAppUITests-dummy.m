854e8be4ed7656c1d344703cef7716ee1aaefd64
尾原徳泰 <nori.tukareta@gmail.com>
2022-06-04 04:05:30 +0900

    uiの実装

---
+++ RxSwiftSampleApp/Pods/Target Support Files/Pods-RxSwiftSampleApp-RxSwiftSampleAppUITests/Pods-RxSwiftSampleApp-RxSwiftSampleAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RxSwiftSampleApp_RxSwiftSampleAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_RxSwiftSampleApp_RxSwiftSampleAppUITests
+@end
---
+++ RxSwiftSampleApp/RxSwiftSampleApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RxSwiftSampleApp
+//
+//  Created by Oh!ara on 2022/06/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var sampleLabel: UILabel!
+    @IBOutlet weak var sampleTextField: UITextField!
+    @IBOutlet weak var sampleButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    func bind() {
+        
+    }
+
+
+}
+
